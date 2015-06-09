65a748134ecfa6cb5a939330ac93b0ccb920f376
Morne de Jongh <mjongh@adaptavist.com>
2015-06-09 11:05:50 +0100

    adding console logs

--- ourwedding/staticserver.js
+++ ourwedding/staticserver.js
@@ -3,4 +3,11 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(80);
+app.listen(80, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
